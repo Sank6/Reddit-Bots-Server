8e73e9020ad38cf169b7fdb3bef8840547981f7a
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-12-31 21:22:43 +0000

    base_url

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,7 +3,7 @@ import {Request, Response, NextFunction} from 'express';
 import * as config from '../../config.json';
 
 export default function(_: Request, res: Response, next: NextFunction) {
-    res.header("Access-Control-Allow-Origin", config.nuxt.baseURL);
+    res.header("Access-Control-Allow-Origin", config.nuxt.base_url);
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 }
