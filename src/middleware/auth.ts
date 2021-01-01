a8494a5d9009735c7a432863262262f6890c5e35
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-01-01 17:08:47 +0000

    Remove status

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 
 export default function(req: Request, res: Response, next: NextFunction) {
-    if (!req.isAuthenticated()) return res.status(403).json({success: false, error: "You aren't signed in."});
+    if (!req.isAuthenticated()) return res.json({success: false, error: "You aren't signed in."});
     else next()
 }
